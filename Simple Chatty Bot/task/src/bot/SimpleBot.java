8917fbc2bcbcf107d6a14323891d93f6bbafbe5e
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-04-10 13:35:41 +0530

    printing my birthyear

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,8 +1,16 @@
+/*
+ * Created by IntelliJ IDEA.
+ *  User: Nandinee Gupta
+ *  Date: 10-April-20
+ *  Time: 1:00 PM
+ */
+
 package bot;
 
 public class SimpleBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Nandinee.");
+        System.out.println("I was created in 2000");
 
     }
 }
